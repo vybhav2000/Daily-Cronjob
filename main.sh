3aa4b22206d0dfc31ee48e07c2812a09bc77995e
Vybhav <vybhav.nca@gmail.com>
2022-06-23 16:25:17 +0530

    Added date for

--- main.sh
+++ main.sh
@@ -3,12 +3,12 @@
 DATE= $(date)
 if [ -f ./datefile.txt ]
 then 
-  echo $DATE >> datefile.txt
+  echo "$DATE" >> datefile.txt
   git add .
   git commit -m "Added date for $DATE"
   git push origin main
 else
-  echo $DATE > datefile.txt
+  echo "$DATE" > datefile.txt
   git add .
   git commit -m "Added date for $DATE"
   git push origin main
