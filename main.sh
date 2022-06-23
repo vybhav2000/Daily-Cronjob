92ccdd27b772ee68021a1251728b0269ac412049
Vybhav Nath C A <55657956+vybhav2000@users.noreply.github.com>
2022-06-23 16:50:13 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,12 +7,12 @@ then
   echo "$(date)" >> datefile.txt
   git add .
   git commit -m "Added date for $DATE"
-  git pull 
+  git pull --rebase
   git push origin main
 else
   echo "$(date)" > datefile.txt
   git add .
   git commit -m "Added date for $DATE"
-  git pull 
+  git pull --rebase
   git push origin main
 fi
