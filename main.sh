913921e2e8fdd35c94bacbcbcfed5d1e249c2d45
Vybhav Nath C A <55657956+vybhav2000@users.noreply.github.com>
2022-06-23 16:44:15 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,16 +2,17 @@
 
 git config --global user.email "vybhav.nca@gmail.com"
 git config --global user.name "vybhav2000"
-git pull --rebase
 if [ -f ./datefile.txt ]
 then 
   echo "$(date)" >> datefile.txt
   git add .
   git commit -m "Added date for $DATE"
+  git pull 
   git push origin main
 else
   echo "$(date)" > datefile.txt
   git add .
   git commit -m "Added date for $DATE"
+  git pull 
   git push origin main
 fi
